bb729a72b64ad3278468d30a9567cc2b29ba5770
nellyilieva <122352770+nellyilieva@users.noreply.github.com>
2024-12-11 22:33:51 +0200

    update

--- Interpolation/lagrange_poly.py
+++ Interpolation/lagrange_poly.py
@@ -13,3 +13,16 @@ def lagrange_poly(f, nodes, x):
         result += f(nodes[i]) * L_i
 
     return result
+
+
+def lagrange_poly(x_nodes, y_nodes, x):
+    n = x_nodes.size
+    result = 0.0
+
+    for i in range(n):
+        l_i = y_nodes[i]
+        for j in range(n):
+            if i != j:
+                l_i *= (x - x_points[j]) / (x_points[i] - x_points[j])
+        result += l_i
+    return result
